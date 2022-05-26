1add66836592bb17ea74798167838ac47a032b5c
Zack Adams <imzackadams@protonmail.com>
2022-05-25 20:28:39 -0400

    feat: add forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
-
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "7704b9726481865dbd0ad45c53c6b7c5"
+
 
 blog = [
     {
@@ -27,6 +30,16 @@ def index():
 def about():
     return render_template('about.html', title="about")
 
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title="Register", form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
